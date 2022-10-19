1ef1f47d867e6339fc707b5c0552e946658db3be
Mike Solomon <mike120982@gmail.com>
2022-10-19 13:05:05 +0300

    Uses mp3 on filestack for better ios bundling

--- io/soundfile.js
+++ io/soundfile.js
@@ -1,10 +1,10 @@
-import halloweenUrl from "../assets/halloween3.mp3";
+const MP3_URL = "https://cdn.filestackcontent.com/wY3jOl5STyGb1ybCYZZC";
 
 export const getAudioData = () =>
   new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
 
-    request.open("GET", halloweenUrl, true);
+    request.open("GET", MP3_URL, true);
 
     request.responseType = "arraybuffer";
 
