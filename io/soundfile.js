c4ac602635f5173d37a5342172a7fdbd05fc0649
Mike Solomon <mike120982@gmail.com>
2022-10-27 09:55:26 +0300

    Falls back to smaller audio track for slower internet speeds

--- io/soundfile.js
+++ io/soundfile.js
@@ -1,10 +1,15 @@
 import halloweenUrl from "../assets/halloween3.mp3";
+import halloween3GUrl from "../assets/mmm1.1.ffmpeg.v8.mp3";
 
 export const getAudioData = () =>
   new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
-
-    request.open("GET", halloweenUrl, true);
+    let myUrl = halloweenUrl;
+    const connection = navigator.connection;
+    if (connection && connection.effectiveType !== "4g") {
+      myUrl = halloween3GUrl;
+    }
+    request.open("GET", myUrl, true);
 
     request.responseType = "arraybuffer";
 
@@ -12,7 +17,9 @@ export const getAudioData = () =>
       const audioData = request.response;
       resolve(audioData);
     };
-    request.onerror = () => { reject("Could not download audio data"); }
+    request.onerror = () => {
+      reject("Could not download audio data");
+    };
 
     request.send();
   });
